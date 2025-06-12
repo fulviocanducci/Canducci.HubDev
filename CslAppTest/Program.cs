9313b395a9caabfd618687a6c368a2f16f40a09d
fulvio <fulviocanducci@hotmail.com>
2025-06-12 09:02:38 -0300

    update now

--- CslAppTest/Program.cs
+++ CslAppTest/Program.cs
@@ -12,10 +12,10 @@ namespace CslAppTest
                 ZipSearch zipSearch = new ZipSearch(hubDev);
                 CpfSearch cpfSearch = new CpfSearch(hubDev);
                 CnpjSearch cnpjSearch = new CnpjSearch(hubDev);
-                var result = zipSearch.GetAsync("01001111").Result;
-                var result2 = cpfSearch.GetAsync("12345678900", new DateTime(1990, 1, 1)).Result;
-                var result3a = cnpjSearch.GetAsync("09.645.389/0001-05", true).Result;
-                var result3 = cnpjSearch.GetAsync("00776574000156", true).Result;
+                var result = zipSearch.Get("19200000");
+                var result2 = cpfSearch.Get("26257821886", new DateTime(1990, 1, 1));
+                var result3a = cnpjSearch.Get("09.645.389/0001-05", true);
+                var result3 = cnpjSearch.Get("00776574000156", true);
             }
             catch (Exception ex)
             {
