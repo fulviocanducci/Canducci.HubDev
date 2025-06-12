57eab49afb89e261a22ad5c1efb7b502d2aa2b95
fulvio <fulviocanducci@hotmail.com>
2025-06-12 09:47:35 -0300

    update now!

---
+++ Canducci.HubDev.Tests/TestAll.cs
@@ -0,0 +1,15 @@
+﻿namespace Canducci.HubDev.Tests
+{
+    [TestClass]
+    public sealed class TestAll
+    {
+        [TestMethod]
+        public void TestMethodCreateInstanceHuvDev()
+        {
+            HubDev hubDev = new HubDev("token");
+            Assert.IsNotNull(hubDev);
+            Assert.AreEqual("token", hubDev.Token);
+            Assert.IsInstanceOfType<HubDev>(hubDev);
+        }
+    }
+}
