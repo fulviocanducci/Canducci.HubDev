b2667e7878881f090bf363794a664433e56032c0
fulvio <fulviocanducci@hotmail.com>
2025-06-20 16:05:49 -0300

    Update Now!

--- Canducci.HubDev/Converters/BrazilianDecimalConverter.cs
+++ Canducci.HubDev/Converters/BrazilianDecimalConverter.cs
@@ -8,18 +8,38 @@ namespace Canducci.HubDev.Converters
         public override decimal? ReadJson(JsonReader reader, Type objectType, decimal? existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
             if (reader.Value == null)
+            {
                 return null;
-
+            }
+            if (reader.Value is decimal decimalValue)
+            {
+                return decimalValue;
+            }
+            if (reader.Value is double doubleValue)
+            {
+                return (decimal)doubleValue;
+            }
+            if (reader.Value is float floatValue)
+            {
+                return (decimal)floatValue;
+            }
+            if (reader.Value is long longValue)
+            {
+                return longValue;
+            }
+            if (reader.Value is int intValue)
+            {
+                return intValue;
+            }
             string valueStr = reader.Value.ToString();
-
-            // Tenta parsear com ponto decimal (formato americano)
             if (decimal.TryParse(valueStr, NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
+            {
                 return result;
-
-            // Tenta com vírgula (formato brasileiro), se necessário
+            }
             if (decimal.TryParse(valueStr, NumberStyles.Any, new CultureInfo("pt-BR"), out result))
+            {
                 return result;
-
+            }
             return null;
         }
 
